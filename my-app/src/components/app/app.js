f05f58cf193230ac22eae04ecb4f99b85db3ca71
Artur Allamyradov <allamuradov@internetlab.ru>
2023-03-16 18:17:15 +0300

    init project

---
+++ my-app/src/components/app/app.js
@@ -0,0 +1,18 @@
+import AppInfo from "../app-info/app-info";
+
+import './app.css';
+
+
+function App () {
+    return (
+      <div className={'App'}>
+          <AppInfo></AppInfo>
+
+          <div className={'search-filter'}>
+
+          </div>
+      </div>
+    );
+}
+
+export default App;
---
+++ my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+
+
+
+const SearchPanel = () => {
+    return (
+        <input
+               type={'text'}
+               className={'form-control search-input'}
+               placeholder={'Find employee'}/>
+    )
+}
+
+export default SearchPanel;
